c30f26a20956501a86c284379220ea18a80700a0
laurenegts <lauren.egts@gmail.com>
2023-03-31 11:01:18 -0400

    removed unused files

--- RatLabPrototype/Scripts/tableview.py
+++
@@ -1,6 +0,0 @@
-@app.route('/tableview')
-def index():
-    rows = Units.query.all()
-    return render_template('tableview.html',
-                            title='Overview',
-                            rows=rows)

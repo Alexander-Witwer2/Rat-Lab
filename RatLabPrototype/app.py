74ea6ce56ce7c87f2c32cf0717599d92b67780a3
laurenegts <lauren.egts@gmail.com>
2023-04-28 16:41:07 -0400

    figured out why search page was running slowly, accidentally had functions to fill in data commented in

--- RatLabPrototype/app.py
+++ RatLabPrototype/app.py
@@ -371,13 +371,13 @@ def search():
     form = FamilyTreeForm()   
     query = db.session.execute(db.select(Rat).order_by(cast(Rat.rat_number, Integer).desc())).scalars()
     
-    updateAges()
-    rats = db.session.execute(db.select(Rat.rat_number, Rat.sire, Rat.dam)).all()
-    for rat in rats:
-        if rat.sire == "EN" and rat.dam == "EN":
-            fillGenealogyData(rat.rat_number, "EN", "EN")
-        else:
-            fillGenealogyData(rat.rat_number, rat.sire, rat.dam)
+    # updateAges()
+    # rats = db.session.execute(db.select(Rat.rat_number, Rat.sire, Rat.dam)).all()
+    # for rat in rats:
+    #     if rat.sire == "EN" and rat.dam == "EN":
+    #         fillGenealogyData(rat.rat_number, "EN", "EN")
+    #     else:
+    #         fillGenealogyData(rat.rat_number, rat.sire, rat.dam)
 
     #print(query.all())
     # Whatever you do, do NOT run print(query.all()) before the return statement
